5ff10369872b1fc930623a9df14161739b6058b1
Taufiq R <taufiq.it2@gmail.com>
2019-10-02 20:01:35 +0700

    fix

---
+++ src/pbo2/pkg10118080/latihan19/saldotabungan/PBO210118080Latihan19SaldoTabungan.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118080.latihan19.saldotabungan;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+/**
+ *
+ * @author 
+ * Nama :Taufiq Rizky
+ * NIM  :10118080
+ * Deskripsi Program    : program saldo tabungan
+ */
+public class PBO210118080Latihan19SaldoTabungan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        DecimalFormat df = (DecimalFormat) DecimalFormat.getCurrencyInstance();
+        DecimalFormatSymbols dfs = new DecimalFormatSymbols();
+        dfs.setCurrencySymbol("");
+        dfs.setMonetaryDecimalSeparator(',');
+        dfs.setGroupingSeparator('.');
+        df.setDecimalFormatSymbols(dfs);
+    
+        double saldo=2500000;
+        String hsl;
+        for (int i = 1; i < 7; i++) {
+            saldo=saldo+(saldo*0.15);
+            hsl = "Rp. " + df.format(saldo);
+            System.out.println("Saldo di bilan ke-"+i+" "+hsl);
+            
+        }
+    }
+    
+}
